fdbc2422cda4a29dd96a722949c51e96c5582ff7
topisteronyango <t.onyango@alustudent.com>
2022-04-21 20:07:15 +0000

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,47 +1,13 @@
 #!/usr/bin/python3
-'''solution to min-operations problem'''
-import math
-
-
-def highestPrimeFactor(n):
-    '''finds highest prime factor of n'''
-    maximum_prime = 0
-
-    while n % 2 == 0:
-        maximum_prime = 2
-        n = n / 2
-
-    for i in range(3, int(math.sqrt(n)) + 1, 2):
-        while n % i == 0:
-            maximum_prime = i
-            n = n / i
-
-    if n > 2:
-        maximum_prime = n
-
-    return int(max_prime)
-
-
-def isPrime(n):
-    ''' checks whether number is prime or not '''
-    flag = False
-
-    if n > 1:
-        for i in range(2, n):
-            if (n % i) == 0:
-                flag = True
-                break
-    return not flag
+""" 0. Minimum Operations """
 
 
 def minOperations(n):
-    ''' finds number of minimum of operations to get n number of H '''
-    if n <= 1:
-        return 0
-
-    if n == 2 or isPrime(n):
-        return n
-
-    hpf = highestPrimeFactor(n)
-    resultOfDivision = n / hpf
-    return int(hpf + resultOfDivision)
+    """ Return: total """
+    t, m = 0, 2
+    while n > 1:
+        while not n % m:
+            t += m
+            n /= m
+        m += 1
+    return t
