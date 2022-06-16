a2bd50ee8d2f1db62058ca89be67745d91de5e87
topisteronyango <t.onyango@alustudent.com>
2022-06-15 22:48:09 -0700

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,10 +2,36 @@
 """ Prime Game  """
 
 
+def isPrime(n):
+    """
+    Returns prime numbers between 1 and n
+    (sieve of Eratosthenes)
+    """
+    primeNums = []
+    sieve = [True] * (n + 1)
+    for num in range(2, n + 1):
+        if sieve[num]:
+            primeNums.append(num)
+            for i in range(num, n + 1, num):
+                sieve[i] = False
+    return primeNums
+
+
 def isWinner(x, nums):
-    """ Returns the winner of the Prime Game """
-    if x == 0 or x == -1:
+    """Function to determine winner of Prime game"""
+    if x is None or nums is None or x == 0 or nums == []:
         return None
-    elif x == 10000:
+    mariaCount = 0
+    benCount = 0
+    for n in range(x):
+        primeNums = isPrime(nums[n])
+        if len(primeNums) % 2 != 0:
+            mariaCount += 1
+        else:
+            benCount += 1
+    if mariaCount > benCount:
         return "Maria"
-    return "Ben"
+    elif benCount > mariaCount:
+        return "Ben"
+    else:
+        return None
